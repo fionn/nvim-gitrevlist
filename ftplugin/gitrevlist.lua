0263b1ad77b92e1971fadc2f79d4b7a6a00002bf
Fionn Fitzmaurice <fionn@github.com>
2026-06-18 21:49:44 +0800

    Use vim.lsp.util's window API instead of nvim_open_win
    It feels a little strange to lean on vim.lsp.util for this, since we
    aren't doing anything related to language servers. But this function
    does much of the lifting that we were doing ourselves, now we no longer
    need to create a buffer, set it to be non-modifiable, pop the last line,
    etc. We are also now able to dismiss the window via mouse events,
    something that didn't happen previously, not sure why.

--- ftplugin/gitrevlist.lua
+++ ftplugin/gitrevlist.lua
@@ -8,40 +8,31 @@ vim.opt_local.commentstring = "# %s"
 ---@param args vim.api.keyset.create_user_command.command_args
 local function hover(args)
     local cwd = vim.fs.dirname(vim.api.nvim_buf_get_name(0))
-    local commit_proc = vim.system({"git", "show", "--format=fuller", args.args},
+    local commit_proc = vim.system({"git", "show", args.args},
                                    {cwd = cwd, text = true})
-
-    local buf = vim.api.nvim_create_buf(false, true)
-    vim.api.nvim_set_option_value("filetype", "git", {buf = buf})
 
     local commit_result = commit_proc:wait()
     if commit_result.code ~= 0 then
-        -- Don't bother handling this, as an expected failure mode is calling
-        -- keywordprg on a non-ref, such as a word in a comment.
+        vim.notify("Not a revision: " .. args.args, vim.log.levels.DEBUG)
         return
     end
 
-    local lines = vim.split(commit_result.stdout, "\n", {trimempty = false})
-    table.remove(lines)
-
-    vim.api.nvim_buf_set_lines(buf, 0, -1, true, lines)
-    vim.api.nvim_set_option_value("modifiable", false, {buf = buf})
-    local win = vim.api.nvim_open_win(buf, false, {
-        relative = "cursor",
-        bufpos = {0, 0},
-        width = 72,
-        height = 20,
-        style = "minimal",
-        title = args.args:sub(1, 7)
-    })
+    local _, win = vim.lsp.util.open_floating_preview(
+        vim.split(commit_result.stdout, "\n"), "git", {
+            width = 72,
+            height = 18,
+            title = args.args:sub(1, 7),
+            title_pos = "left"
+        }
+    )
     if win == 0 then
-        vim.notify("Failed to open commit window", vim.log.levels.ERROR)
+        vim.notify("Failed to open commit preview", vim.log.levels.ERROR)
     end
 end
 
-local user_command = "Keywordprg"
-vim.api.nvim_create_user_command(user_command, hover,
+local ex_command = "Keywordprg"
+vim.api.nvim_create_user_command(ex_command, hover,
                                  {desc = "git rev-list keywordprg", nargs = 1})
-vim.opt_local.keywordprg = ":" .. user_command
+vim.opt_local.keywordprg = ":" .. ex_command
 
 vim.b.did_ftplugin = 1
